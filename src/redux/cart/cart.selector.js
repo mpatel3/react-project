f4c4cdfe28de7ef219a5d4461cb6b9d049906f12
mpatel3 <14mcei14@nirmauni.ac.in>
2021-02-07 22:56:48 +0530

    redux selectors for memoization and performance

---
+++ src/redux/cart/cart.selector.js
@@ -0,0 +1,27 @@
+import { createSelector } from 'reselect';
+
+//first type - input selector - does not use createSelector.
+const selectCart = state => state.cart;
+
+
+//second type - output selector - does use input Selector and CreateSelector to build them seleves.
+
+// first argument is an array of input selectors
+// second argument is going to be a function that will return a value we want out of selectors.
+export const selectCartItems = createSelector(
+    [selectCart],
+    // argument each output of input selectors
+    cart => cart.cartItems
+);
+
+// Total Quantity
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems => {
+        return cartItems.reduce(
+            (accumulateQuantity, carItem) =>
+                accumulateQuantity + carItem.quantity,
+                0
+        )   
+    }
+)
