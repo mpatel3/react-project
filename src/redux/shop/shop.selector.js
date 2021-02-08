5329abd94f2ff799685a94bbb44adc3155c4f2bf
mpatel3 <14mcei14@nirmauni.ac.in>
2021-02-08 21:55:04 +0530

    change shop list in redux

---
+++ src/redux/shop/shop.selector.js
@@ -0,0 +1,28 @@
+import {createSelector} from 'reselect';
+
+const selectShop = state => state.shop;
+const COLLECTION_ID_MAP = {
+    "hats": 1,
+    "sneakers": 2,
+    "jackets": 3,
+    "womens": 4,
+    "mens": 5,
+}
+
+export const selectShopCollection =  createSelector(
+    [selectShop],
+    shop => shop.collections
+)
+
+export const selectShopCollectionItem = collectionID => {
+    // return createSelector(
+    //     [selectShopCollection],
+    //     collections => collections.filter(collection => collection.id === COLLECTION_ID_MAP[collectionID])
+    // )
+    // with data normlization - converting array into an object.
+    return createSelector(
+        [selectShopCollection],
+        collections => collections[collectionID]
+    )
+    
+}
