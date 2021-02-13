bc275c72966df4d5b0b3fe58067b6734a8689b84
mpatel3 <14mcei14@nirmauni.ac.in>
2021-02-13 18:51:34 +0530

    styled component - cart total items

--- src/components/cart-icon/car-icon.component.jsx
+++ src/components/cart-icon/car-icon.component.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { FaCartArrowDown } from 'react-icons/fa';
 import { Icon } from '@chakra-ui/react';
-import {selectCartItemsCount} from './../../redux/cart/cart.selector';
+import { selectCartItemsCount } from './../../redux/cart/cart.selector';
+import { CartIconSpanContainer } from './cart-icon.styles';
 
-// import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-import './cart-icon.styles.css';
 import { connect } from 'react-redux';
 
 const CartIcon = ({onOpen, itemsCount}) => {
-    console.log(itemsCount)
     return (
         <>
-        <Icon as={FaCartArrowDown} boxSize={9} />
-        <span className="item-count" onClick={onOpen}>{itemsCount}</span>
+            <Icon as={FaCartArrowDown} boxSize={9} />
+            <CartIconSpanContainer onClick={onOpen}>{itemsCount}</CartIconSpanContainer>
         </>
     )
 }
---
+++ src/components/cart-icon/cart-icon.styles.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const CartIconSpanContainer = styled.span`
+    position: absolute;
+    top: 6px;
+    right: 14px;
+    background: red;
+    text-align: center;
+    border-radius: 7px;
+    cursor:pointer;
+    width: 20px;
+    height: 20px;
+    background-color: #ff6161;
+    border: 1px solid #fff;
+    font-weight: 400;
+    color: #f0f0f0;
+    line-height: 16px;
+    font-size: 12px;
+`;
