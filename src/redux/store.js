d7b8d0848a1d4fe45d1676aa24e2811d7af2fcbd
mpatel3 <14mcei14@nirmauni.ac.in>
2021-02-15 22:38:14 +0530

    Redux Thunk Integration

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,13 @@ import { createStore, applyMiddleware } from "redux";
 import { persistStore } from 'redux-persist';
 import rootReducer from './root.reducer.js';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
+const middlewares = [thunk];
 
-
-const middlewares = [logger];
+if(process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
