dbf869b59e79cbc5e13d0ec97d570087e58bdba6
mpatel3 <14mcei14@nirmauni.ac.in>
2021-02-15 22:42:01 +0530

    Shop Page - Reducer and Actions

--- src/redux/shop/shop.action.js
+++ src/redux/shop/shop.action.js
@@ -1,6 +1,33 @@
 import { ShopActionTypes } from './shop.types';
+import { firestore, collectionSnapShottoMap } from '../../firebase/firebase.utils';
 
-export const updateShopCollections = shopCollectoinMap => ({
-    type: ShopActionTypes.UDATE_COLLECTIONS,
+export const fetchShopCollectionSuccess = shopCollectoinMap => ({
+    type: ShopActionTypes.FETCH_SHOPCOLLECTION_SUCCESS,
     payload: shopCollectoinMap
 });
+
+
+export const fetchShopCollectionStart = () => ({
+    type: ShopActionTypes.FETCH_SHOPCOLLECTION_START,
+})
+
+export const fetchShopCollectionFailure = (err) => ({
+    type: ShopActionTypes.FETCH_SHOPCOLLECTION_FAILURE,
+    payload: err.message
+})
+
+// Redux Thunk Usage.
+export const fetchShopCollectionStartAsync = () => dispatch => {
+    const collectionRef = firestore.collection('shop');
+    dispatch(fetchShopCollectionStart());
+    
+    collectionRef
+    .get()
+    .then(snapShot => {
+        const shopCollectionMap = collectionSnapShottoMap(snapShot);
+        dispatch(fetchShopCollectionSuccess(shopCollectionMap));
+    })
+    .catch(err => {
+        dispatch(fetchShopCollectionFailure(err));
+    })
+}
