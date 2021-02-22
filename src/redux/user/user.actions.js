2e4471ed3e7c45ef6c7ad3c6ee68378adaebcbdf
mpatel3 <14mcei14@nirmauni.ac.in>
2021-02-22 22:54:15 +0530

    New user actions for sagas and redux

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,8 +1,56 @@
 import userTypes from './user.types';
 
-export const setCurrentUser = user => {
+/**
+ * export const setCurrentUser = user => {
     return {
         type: userTypes.setCurrentUser,
         payload: user
     }
 }
+*/
+
+export const googleSignInStart = () => ({
+    type: userTypes.GOOGLE_SIGN_IN_START
+});
+
+export const googleSignInSuccess = (user) => ({
+    type: userTypes.GOOGLE_SIGN_IN_SUCCESS,
+    payload: user
+});
+
+export const googleSignInFailure = (error) => ({
+    type: userTypes.GOOGLE_SIGN_IN_FAILURE,
+    payload: error
+});
+
+export const emailSignInStart = emailAndPassword => ({
+    type: userTypes.EMAIL_SIGN_IN_START,
+    payload: emailAndPassword
+});
+
+export const emailSignInSuccess = (user) => ({
+    type: userTypes.EMAIL_SIGN_IN_SUCCESS,
+    payload: user
+});
+
+export const emailSignInFailure = (error) => ({
+    type: userTypes.EMAIL_SIGN_IN_FAILURE,
+    payload: error
+});
+
+export const checkUserSession = () => ({
+    type: userTypes.CHECK_USER_SESSION
+});
+
+export const signOutStart = () => ({
+    type: userTypes.SIGN_OUT_START
+});
+
+export const signOutSuccess = () => ({
+    type: userTypes.SIGN_OUT_SUCCESS
+});
+
+export const signOutFailure = (error) => ({
+    type: userTypes.SIGN_OUT_FAILURE,
+    payaload: error
+});
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,13 +1,36 @@
 import userTypes from "./user.types";
 
 const INITIAL_STATE = {
-    currentUser: null
+    currentUser: null,
+    error: null
 }
 
 const userReducer = (state=INITIAL_STATE, action) => {
     switch(action.type) {
-        case userTypes.setCurrentUser:
-            return {...state, currentUser: action.payload}
+        case userTypes.GOOGLE_SIGN_IN_SUCCESS:
+        case userTypes.EMAIL_SIGN_IN_SUCCESS:
+            return {
+                ...state, 
+                currentUser: action.payload,
+                error: null,
+            }
+        case userTypes.GOOGLE_SIGN_IN_SUCCESS:
+        case userTypes.EMAIL_SIGN_IN_SUCCESS:
+            return {
+                ...state, 
+                error: action.payload
+            }
+        case userTypes.SIGN_OUT_FAILURE:
+            return {
+                ...state,
+                error: action.payload
+            }
+        case userTypes.SIGN_OUT_SUCCESS:
+            return {
+                ...state,
+                currentUser: null,
+                error: null
+            }
         default:
             return state;
     }
